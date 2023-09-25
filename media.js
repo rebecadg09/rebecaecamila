43862a2f85f303a4540e27acfc4e1f204f9a0ab3
rebecadg09 <113352593+rebecadg09@users.noreply.github.com>
2023-09-25 07:54:29 -0300

    250923

--- media.js
+++ media.js
@@ -2,22 +2,23 @@ const form = document.querySelector("form")
 const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
 
-form.addEventiListener("submit",(e)=>{
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+e.preventDefault()
 
-    const nome = form.inNome.value
-    const nota1 = Number(form.inNota1.value)
-    const nota2 = Number(form.inNota2.value)
-    const media = (nota1 + nota2)/2
-    resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+const nome = form.inNome.value
+const nota1 = Number(form.inNota1.value)
+const nota2 = Number(form.inNota2.value)
+const media = (nota1 + nota2)/2
+resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+
+if (media >= 7) {
+    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
+    resp2.style.color = "blue"
+} else if (media >= 4) {
+    resp2.innerText = `Atenção ${nome}! Você está de exame`
+    resp2.style.color = "green"
+} else {
+    resp2.innerText = `Ops ${nome}! Você foi reprovado(a)`
+    resp2.style.color = "red"
+}
 })
- if(media>=7) {
-    resp2.innerText = 'Parabéns ${nome}! Você foi aprovado(a)'
-    resp2.style.color= "blue"
- } else if (media>=4) {
-    resp.innerText = 'Atenção ${nome}! Você está de exame'
-    resp2.style.color= "green"
- } else {
-    resp2.innerText= 'Ops ${nome}! Você foi reprovado(a)'
-    reesp2.style.color = "red"
- }
